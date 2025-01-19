5d02116c9976dbdcfb5f4503bf42a27ab9d09425
fernandozambone <fernando.zambone@gmail.com>
2025-01-19 16:43:50 -0600

    Handles database ssl connection

--- infra/database.js
+++ infra/database.js
@@ -7,14 +7,16 @@ async function query(sql) {
     user: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
     database: process.env.POSTGRES_DB,
+    ssl: process.env.NODE_ENV === "development" ? false : true,
   });
 
-  await client.connect();
   try {
+    await client.connect();
     const result = await client.query(sql);
     return result;
   } catch (error) {
     console.error(error);
+    throw error;
   } finally {
     await client.end();
   }
